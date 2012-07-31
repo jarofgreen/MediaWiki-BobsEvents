38687f6d97c3d3b3be7ce86ccc6face29985af57
James <james@jarofgreen.co.uk>
2012-07-31 08:28:12 +0100

    bugfix, events might not have summaries now

--- create.mysql.sql
+++ create.mysql.sql
@@ -2,7 +2,7 @@ CREATE TABLE events (
 	page_id INT UNSIGNED NOT NULL DEFAULT 0,
 	start_at INT UNSIGNED NOT NULL,
 	end_at INT UNSIGNED NOT NULL,
-	summary TEXT NOT NULL,
+	summary TEXT NULL,
 	url VARCHAR(255) NULL,
 	deleted BOOLEAN DEFAULT 0,
 	KEY date_idx (page_id,start_at)
